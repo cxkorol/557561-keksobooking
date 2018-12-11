dc3a25128e36653f6c1dcc106c72dbe59dd8478c
korol <cx.korol@gmail.com>
2018-12-11 15:00:36 +0300

    Валидация форм со всеми исправлениями

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@
     palace: 'Дворец',
     flat: 'Квартира',
     house: 'Дом',
-    bungalo: 'Бунагло'
+    bungalo: 'Бунгало'
   };
 
   var OFFER_TIME = [
@@ -272,6 +272,7 @@
     var inputAdress = adForm.querySelector('#address');
 
     inputAdress.setAttribute('value', locationX + ', ' + locationY);
+    inputAdress.setAttribute('readonly', 'readonly');
   };
 
   // Функция перехода в активное состояние
@@ -337,4 +338,64 @@
       closeCardPopup();
     }
   });
+
+  // Модуль 4.2
+
+  var inputPrice = adForm.querySelector('#price');
+  var inputType = adForm.querySelector('#type');
+  var capacity = adForm.querySelector('#capacity');
+  var inputOption = capacity.querySelectorAll('option');
+  var inputRoom = adForm.querySelector('#room_number');
+  var inputTimeIn = adForm.querySelector('#timein');
+  var inputTimeOut = adForm.querySelector('#timeout');
+
+  var getPrice = function (evt) {
+    var value = evt.target.value;
+    switch (value) {
+      case 'bungalo':
+        inputPrice.min = 0;
+        inputPrice.value = 0;
+        break;
+      case 'house':
+        inputPrice.min = 5000;
+        inputPrice.value = 5000;
+        break;
+      case 'flat':
+        inputPrice.min = 1000;
+        inputPrice.value = 1000;
+        break;
+      case 'palace':
+        inputPrice.min = 10000;
+        inputPrice.value = 10000;
+        break;
+    }
+  };
+
+  var guestQuantity = {
+    '1': [true, true, false, true],
+    '2': [true, false, false, true],
+    '3': [false, false, false, true],
+    '100': [true, true, true, false],
+  };
+
+  var roomNumberHandler = function (evt) {
+    var value = evt.target.value;
+    for (var i = 0; i < inputOption.length; i++) {
+      inputOption[i].disabled = guestQuantity[value][i];
+      if (inputOption[i].disabled === true) {
+        inputOption[i].selected = false;
+      }
+    }
+  };
+
+  inputType.addEventListener('input', getPrice);
+  inputRoom.addEventListener('change', roomNumberHandler);
+
+  inputTimeIn.addEventListener('change', function (evt) {
+    inputTimeOut.value = evt.target.value;
+  });
+  inputTimeOut.addEventListener('change', function (evt) {
+    inputTimeIn.value = evt.target.value;
+  });
+
 })();
